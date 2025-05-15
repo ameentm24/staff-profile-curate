5b64bd819b6cccda4272735c1ba4ab517b226782
ameentm24 <toameentm@gmail.com>
2025-05-15 11:49:44 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "vite-plugin-vercel";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel(), tailwindcss(), react()],
+
+  plugins: [ tailwindcss(), react()],
 });
